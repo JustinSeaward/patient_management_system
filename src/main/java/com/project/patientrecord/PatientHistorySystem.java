3c3c6e70ad0462f4dad9b4a6f39ae90c38ef2e7f
Justin Seaward <jut_seaward@yahoo.com>
2026-07-04 15:58:49 -0230

    PatientHistorySystem: added methods to insert new patient records, display records and navigated through the records

--- src/main/java/com/project/patientrecord/PatientHistorySystem.java
+++ src/main/java/com/project/patientrecord/PatientHistorySystem.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 public class PatientHistorySystem {
     private PatientRecord head;
     private PatientRecord tail;
+    private PatientRecord currentView;
     private int size;
 
     public PatientRecord createPatientRecord(LocalDate visitData, String diagnosis, String treatmentNotes){
@@ -17,10 +18,57 @@ public class PatientHistorySystem {
 
     public void insertPatientRecord(LocalDate visitDate, String diagnosis, String treatmentNotes){
         PatientRecord newRecord = new PatientRecord(visitDate, diagnosis, treatmentNotes);
-
         if (head == null){
             createPatientRecord(visitDate, diagnosis, treatmentNotes);
             return;
         }
+        newRecord.setPrevious(tail);
+        tail.setNext(newRecord);
+        tail = newRecord;
+        size++;
+    }
+
+    public void displayNewestPatientRecord(){
+        if(tail == null){
+            System.out.println("No record to display");
+            return;
+        }
+        currentView = tail;
+        System.out.println(currentView.getVisitDate());
+        System.out.println(currentView.getDiagnosis());
+        System.out.println(currentView.getTreatmentNotes());
+    }
+
+    public void displayOldestPatientRecord(){
+        if(head == null){
+            System.out.println("No record to display");
+            return;
+        }
+        currentView = head;
+        System.out.println(currentView.getVisitDate());
+        System.out.println(currentView.getDiagnosis());
+        System.out.println(currentView.getTreatmentNotes());
+    }
+
+    public void moveToNextRecord() {
+        if(tail == currentView){
+            System.out.println("You have reached newest patient record");
+            return;
+        }
+        currentView = currentView.getNext();
+        System.out.println(currentView.getVisitDate());
+        System.out.println(currentView.getDiagnosis());
+        System.out.println(currentView.getTreatmentNotes());
+    }
+
+    public void moveToPreviousPatientRecord(){
+        if(head == currentView){
+            System.out.println("You have reach the oldest patient record");
+            return;
+        }
+        currentView = currentView.getPrevious();
+        System.out.println(currentView.getVisitDate());
+        System.out.println(currentView.getDiagnosis());
+        System.out.println(currentView.getTreatmentNotes());
     }
 }
